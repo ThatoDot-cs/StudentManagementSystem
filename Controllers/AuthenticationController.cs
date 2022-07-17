48048aafd7a03d361a680e69b667b3571690c8ef
ThatoDot-cs <mcpta.khumza@gmail.com>
2022-07-17 12:10:23 +0200

    Added Login & SignUp Views & Auth Controller

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class AuthenticationController: Controller
+    {
+        //GET: /Authentication
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /Authentication/Welcome
+        public IActionResult Welcome(string name, int numTimes)
+        {
+            //return "This is my welcome action method";
+            return View();
+        }
+
+        //GET: /Authentication/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Models/SignUpViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.Models
+{
+    public class SignUpViewModel
+    {
+        private string? _UserName { get; }
+        public int ID { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+        public static void GenerateUsername()
+        {
+
+        }
+    }
+}
